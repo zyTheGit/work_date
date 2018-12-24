af0e55a4c18e1ee82b24b429d3f0576bed361166
zy <1353969708@qq.com>
2018-12-24 16:47:07 +0800

    1

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
          this.yearList = [];
          this.monthlyList = [];
          this.dayList = [];
-         this.statusContext="正常班";
+         this.statusContext = "正常班";
          this.yearValue = "";
          this.monthValue = "";
          this.attendArray = []; //返回排班内容,status什么班,shiftsRule班次规则
@@ -21,7 +21,7 @@
          this.cardShelves = null; //卡片
          this.statusContent = ""; //返回卡片状态
          this.dateFailValue = "";
-         this.currentDate();
+         this.arrayFormOrforEach();
      }
      init() {
          this.currentDate();
@@ -134,11 +134,11 @@
              if (this.isNoDisabledBtn && this.currentObj.year >= year) {
                  if (this.currentObj.year == year && this.currentObj.month >= month) {
                      if (this.currentObj.month == month) {
-                         if( this.currentObj.day > i + 1){
-                            disabledBtn = " disabledBtn";
+                         if (this.currentObj.day > i + 1) {
+                             disabledBtn = " disabledBtn";
                          }
-                     } else{
-                        disabledBtn = " disabledBtn";
+                     } else {
+                         disabledBtn = " disabledBtn";
                      }
                  } else {
                      disabledBtn = " disabledBtn";
@@ -216,17 +216,17 @@
          let This = this;
          ulYearLi.forEach(li => {
              li.addEventListener("click", function (e) {
-                This.changenClick=true;
+                 This.changenClick = true;
                  ulYearLi.forEach(removeClassLi => {
                      removeClassLi.setAttribute("class", "");
                  });
                  This.yearValue = parseInt(this.childNodes[0].innerText);
                  this.setAttribute("class", "calendar_active");
                  if (!!This.changeEventCallback) {
-                    This.changeEventCallback(This);
-                }else{
-                    This.createDay();
-                }
+                     This.changeEventCallback(This);
+                 } else {
+                     This.createDay();
+                 }
              });
          });
      }
@@ -236,18 +236,18 @@
          let This = this;
          ulMonthLi.forEach(li => {
              li.addEventListener("click", function (e) {
-                 This.changenClick=true;
+                 This.changenClick = true;
                  ulMonthLi.forEach(removeClassLi => {
                      removeClassLi.setAttribute("class", "");
                  });
                  This.monthValue = parseInt(this.childNodes[0].innerText);
                  this.setAttribute("class", "calendar_active");
-                 
-                if (!!This.changeEventCallback) {
-                    This.changeEventCallback(This);
-                }else{
-                    This.createDay();
-                }
+
+                 if (!!This.changeEventCallback) {
+                     This.changeEventCallback(This);
+                 } else {
+                     This.createDay();
+                 }
              });
          });
      }
@@ -275,7 +275,7 @@
              });
          });
          document.onclick = () => {
-            this.changenClick=false;
+             this.changenClick = false;
              let ulList = document.querySelectorAll(".calendar_header ul");
              ulList.forEach(ul => {
                  ul.style.display = 'none';
@@ -319,6 +319,119 @@
                  return false;
              });
          });
+     }
+
+     arrayFormOrforEach(){
+        if (!Array.prototype.forEach) {
+
+            Array.prototype.forEach = function (callback, thisArg) {
+        
+                var T, k;
+        
+                if (this == null) {
+                    throw new TypeError(' this is null or not defined');
+                }
+                var O = Object(this);
+                var len = O.length >>> 0;
+        
+                if (typeof callback !== "function") {
+                    throw new TypeError(callback + ' is not a function');
+                }
+                if (arguments.length > 1) {
+                    T = thisArg;
+                }
+    
+                k = 0;
+        
+                while (k < len) {
+        
+                    var kValue;
+                    if (k in O) {
+                        kValue = O[k];
+                        callback.call(T, kValue, k, O);
+                    }
+                    k++;
+                }
+            };
+        }
+         if (!Array.from) {
+           Array.from = (function () {
+             var toStr = Object.prototype.toString;
+             var isCallable = function (fn) {
+               return typeof fn === 'function' || toStr.call(fn) === '[object Function]';
+             };
+             var toInteger = function (value) {
+               var number = Number(value);
+               if (isNaN(number)) { return 0; }
+               if (number === 0 || !isFinite(number)) { return number; }
+               return (number > 0 ? 1 : -1) * Math.floor(Math.abs(number));
+             };
+             var maxSafeInteger = Math.pow(2, 53) - 1;
+             var toLength = function (value) {
+               var len = toInteger(value);
+               return Math.min(Math.max(len, 0), maxSafeInteger);
+             };
+         
+             // The length property of the from method is 1.
+             return function from(arrayLike/*, mapFn, thisArg */) {
+               // 1. Let C be the this value.
+               var C = this;
+         
+               // 2. Let items be ToObject(arrayLike).
+               var items = Object(arrayLike);
+         
+               // 3. ReturnIfAbrupt(items).
+               if (arrayLike == null) {
+                 throw new TypeError("Array.from requires an array-like object - not null or undefined");
+               }
+         
+               // 4. If mapfn is undefined, then let mapping be false.
+               var mapFn = arguments.length > 1 ? arguments[1] : void undefined;
+               var T;
+               if (typeof mapFn !== 'undefined') {
+                 // 5. else      
+                 // 5. a If IsCallable(mapfn) is false, throw a TypeError exception.
+                 if (!isCallable(mapFn)) {
+                   throw new TypeError('Array.from: when provided, the second argument must be a function');
+                 }
+         
+                 // 5. b. If thisArg was supplied, let T be thisArg; else let T be undefined.
+                 if (arguments.length > 2) {
+                   T = arguments[2];
+                 }
+               }
+         
+               // 10. Let lenValue be Get(items, "length").
+               // 11. Let len be ToLength(lenValue).
+               var len = toLength(items.length);
+         
+               // 13. If IsConstructor(C) is true, then
+               // 13. a. Let A be the result of calling the [[Construct]] internal method 
+               // of C with an argument list containing the single item len.
+               // 14. a. Else, Let A be ArrayCreate(len).
+               var A = isCallable(C) ? Object(new C(len)) : new Array(len);
+         
+               // 16. Let k be 0.
+               var k = 0;
+               // 17. Repeat, while k < len… (also steps a - h)
+               var kValue;
+               while (k < len) {
+                 kValue = items[k];
+                 if (mapFn) {
+                   A[k] = typeof T === 'undefined' ? mapFn(kValue, k) : mapFn.call(T, kValue, k);
+                 } else {
+                   A[k] = kValue;
+                 }
+                 k += 1;
+               }
+               // 18. Let putStatus be Put(A, "length", len, true).
+               A.length = len;
+               // 20. Return A.
+               return A;
+             };
+           }());
+         }
+        
      }
  }
  window.CreateDateControl = CreateDateControl;
